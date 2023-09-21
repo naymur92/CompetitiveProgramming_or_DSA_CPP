6a3cdf31b010e308f8ec676c15e29f30566c1aae
Naymur Rahman <naymurrahman@yahoo.com>
2023-09-21 18:54:16 +0600

    add insertion sort programm

---
+++ solutions/insertion_sort.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	 int size = 10000;
+    int max_value = 999999;
+
+    // Generate the array of random integers
+    int random_integers[size];
+    for (int i = 0; i < size; ++i) {
+        random_integers[i] = rand() % (max_value + 1); // Generates random numbers between 0 and max_value
+    }
+
+
+    // start sorting programm
+    for(int i = 1; i < size; ++i) {
+    	int key = random_integers[i];
+
+    	int j = i - 1;
+    	while(j >= 0 && random_integers[j] > key) {
+    		random_integers[j + 1] = random_integers[j];
+    		j = j - 1;
+    	}
+    	random_integers[j + 1] = key;
+    }
+    // end sorting programm
+
+
+    for(int i = 0; i < size; ++i) {
+    	cout << random_integers[i] << endl;
+    }
+
+	return 0;
+}
