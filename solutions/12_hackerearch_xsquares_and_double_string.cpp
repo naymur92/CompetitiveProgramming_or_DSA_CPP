55d8d45c7a15bbde8cdd92aa414fd4d40afbcc09
Naymur Rahman <naymurrahman@yahoo.com>
2024-01-15 08:33:43 +0600

    practice module M ans, pre computation technique

---
+++ solutions/12_hackerearch_xsquares_and_double_string.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		string s;
+		cin >> s;
+		
+		int str_count[26] = {0};
+
+		for (int i = 0; i < s.size(); ++i)
+		{
+			str_count[s[i] - 'a']++;
+		}
+
+		int found = 0;
+		for (int i = 0; i < 26; ++i)
+		{
+			if(str_count[i] > 1)
+			{
+				found++;
+				break;
+			}
+		}
+		if(found) {
+			cout << "Yes" << endl;
+		} else {
+			cout << "No" << endl;
+		}
+	}
+}
