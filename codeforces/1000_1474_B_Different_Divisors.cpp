03a2c3c4885cd6540b01de06a34d9fbc3be47dd2
Naymur Rahman <naymurrahman@yahoo.com>
2024-07-26 08:36:09 +0600

    solved cf 1474B problem

--- codeforces/1000_1474_B_Different_Divisors.cpp
+++ codeforces/1000_1474_B_Different_Divisors.cpp
@@ -8,6 +8,16 @@ using namespace std;
 
 typedef long long ll;
 
+bool isPrime(int num) {
+	if (num == 1) return false;
+
+	for (int i = 2; i * i <= num; ++i)
+	{
+		if (num % i == 0) return false;
+	}
+	return true;
+}
+
 int main(){
 	int t;
 	cin >> t;
@@ -16,12 +26,20 @@ int main(){
 	{
 		int d;
 		cin >> d;
-		int first = 1;
-		int second = first + d;
-		int third = second + d;
-		int fourth = first * second * third;
 
-		cout << fourth << endl;
+		int num = 1;
+		ll result = 1;
+		for (int i = 0; i < 2; ++i)
+		{
+			num += d;
+
+			// check prime and increase until it is prime
+			while (!isPrime(num)) num++;
+
+			result *= (ll) num;
+		}
+
+		cout << result << "\n";
 	}
 }
 
