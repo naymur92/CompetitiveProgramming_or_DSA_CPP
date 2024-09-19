96b48a98ddcead8d404bea55bad3fd3d7323a9a3
Naymur Rahman <naymurrahman@yahoo.com>
2024-09-19 08:28:42 +0600

    linked list implementation, added insertAtEnd, insertAtNthPosition, deleteAtNthPosition algos

--- DSA/DataStructures/linked_list.cpp
+++ DSA/DataStructures/linked_list.cpp
@@ -19,6 +19,58 @@ void insertAtBegenning(Node* &head, int data) {
 	head = tempNode;
 }
 
+// insert at last
+void insertAtEnd(Node* &head, int data) {
+	auto newNode = new Node();
+	newNode->data = data;
+	newNode->next = nullptr;
+
+	if (head == nullptr) head = newNode;
+	else {
+		auto temp1 = head;
+		while (temp1 != nullptr) {
+			if (temp1->next == nullptr) {
+				temp1->next = newNode;
+				break;
+			}
+			temp1 = temp1->next;
+		}
+	}
+}
+
+// insert a node at n-th position
+void insertAtNthPosition(Node* &head, int data, int n) {
+	auto newNode = new Node();
+	newNode->data = data;
+	newNode->next = nullptr;
+
+	if (n == 1) {
+		newNode->next = head;
+		head = newNode;
+		return;
+	}
+
+	auto temp = head;
+	for (int i = 0; i < n - 2; ++i) temp = temp->next;
+	newNode->next = temp->next;
+	temp->next = newNode;
+}
+
+// delete a node at n-th position
+void deleteAtNthPosition(Node* &head, int n) {
+	auto temp = head;
+	if (n == 1) {
+		head = temp->next;
+		delete temp;	// free(temp);
+		return;
+	}
+
+	for (int i = 0; i < n - 2; ++i) temp = temp->next;
+	auto temp1 = temp->next;
+	temp->next = temp1->next;
+	delete temp1;
+}
+
 // traverse Linked List
 void traverseList(Node* head) {
 	Node* temp = head;
@@ -36,9 +88,24 @@ int main() {
 
 
 	// inserting first element
-	insertAtBegenning(head, 22);
+	// insertAtBegenning(head, 22);
 
-	
+	// inserting elements at the end
+	// insertAtEnd(head, 2);
+	// insertAtEnd(head, 4);
+	// insertAtEnd(head, 6);
+
+	// inserting at n-th position
+	insertAtNthPosition(head, 2, 1);	// 2
+	insertAtNthPosition(head, 4, 2);	// 2 4
+	insertAtNthPosition(head, 6, 1);	// 6 2 4
+	insertAtNthPosition(head, 5, 2);	// 6 5 2 4
+	insertAtNthPosition(head, 8, 3);	// 6 5 8 2 4
+
 	// traverse
+	traverseList(head);
+
+	deleteAtNthPosition(head, 3);	// 6 5 2 4
+	deleteAtNthPosition(head, 1);	// 5 2 4
 	traverseList(head);
 }
