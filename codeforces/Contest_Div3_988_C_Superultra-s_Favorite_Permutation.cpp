3b169a136c308327714ee22e7f89396c4db65fe9
Naymur Rahman <naymurrahman@yahoo.com>
2024-11-19 12:14:21 +0600

    solved a cf problem

--- codeforces/Contest_Div3_988_C_Superultra-s_Favorite_Permutation.cpp
+++ codeforces/Contest_Div3_988_C_Superultra-s_Favorite_Permutation.cpp
@@ -10,7 +10,26 @@ int main() {
     int t;
     cin >> t;
     while(t--) {
-        
+        int n;
+        cin >> n;
+
+        if (n < 5) {
+            cout << -1 << "\n";
+        } else {
+            for (int i = 1; i <= n; i+=2) {
+                if (i == 5) continue;
+                cout << i << " ";
+            }
+
+            cout << "5 4 ";
+
+            for (int i = 2; i <= n; i+=2) {
+                if (i == 4) continue;
+                cout << i << " ";
+            }
+
+            cout << "\n";
+        }        
     }
 }
 
