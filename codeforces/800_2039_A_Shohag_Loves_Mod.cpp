5bc1bd5fa216b9120c00496f898f65b9cf4c1f72
Naymur Rahman <naymurrahman@yahoo.com>
2024-12-02 09:09:23 +0600

    solved 2039A, 2031B problems from cf

---
+++ codeforces/800_2039_A_Shohag_Loves_Mod.cpp
@@ -0,0 +1,38 @@
+/*
+https://codeforces.com/problemset/problem/2039/A
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {	
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int n;
+		cin >> n;
+
+		for (int i = 0; i < n; ++i)
+		{
+			cout << (2 * i + 1) << " ";
+		}
+		cout << "\n";
+	}
+}
+
+
+/*
+2
+3
+6
+
+*/
+
+/*
+2 7 8
+2 3 32 35 69 95
+
+*/
---
+++ codeforces/900_2031_B_Penchick_and_Satay_Sticks.cpp
@@ -0,0 +1,54 @@
+/*
+https://codeforces.com/problemset/problem/2039/A
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {	
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		int a[n];
+
+		for (int i = 0; i < n; ++i)
+		{
+			cin >> a[i];
+		}
+
+		bool is_possible = true;
+		for (int i = 0; i < n - 1; ++i)
+		{
+			if (a[i] == i + 1) continue;
+
+			if (a[i + 1] != i + 1 || a[i] != i + 2) {
+				is_possible = false;
+				break;
+			}
+			swap(a[i], a[i + 1]);
+		}
+
+		cout << (is_possible ? "YES\n" : "NO\n");
+	}
+}
+
+
+/*
+2
+4
+2 1 3 4
+4
+4 2 3 1
+
+*/
+
+/*
+YES
+NO
+
+*/
