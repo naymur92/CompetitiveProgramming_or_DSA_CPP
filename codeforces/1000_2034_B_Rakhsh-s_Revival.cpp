e6b7338f25877025a3697988205fb99ec8bdf08f
Naymur Rahman <naymurrahman@yahoo.com>
2024-12-17 08:43:32 +0600

    solving a cf problem

---
+++ codeforces/1000_2034_B_Rakhsh-s_Revival.cpp
@@ -0,0 +1,52 @@
+/*
+https://codeforces.com/problemset/problem/2034/B
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n, m, k;
+		cin >> n >> m >> k;
+		string s;
+		cin >> s;
+
+		int ans = 0;
+		for (int i = 0; i < n; ++i) {
+			if (s[i] == '1') continue;
+
+			int j = i;
+			while (j < n and s[j] == '0') j++;
+
+			ans += (j - i);
+
+			cout << j << " ";
+		}
+		cout << "\n";
+		cout << ans << "\n";
+	}
+}
+
+
+/*
+3
+5 1 1
+10101
+5 2 1
+10101
+6 3 2
+000000
+
+*/
+
+/*
+2
+0
+1
+
+*/
