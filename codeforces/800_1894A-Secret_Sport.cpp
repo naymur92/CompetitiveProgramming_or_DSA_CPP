2782bbdef5c3feab9549d607c9bfedacd2d5b1c9
Naymur Rahman <naymurrahman@yahoo.com>
2025-02-19 07:57:50 +0600

    solved codeforces 1894A problem

---
+++ codeforces/800_1894A-Secret_Sport.cpp
@@ -0,0 +1,58 @@
+/*
+https://codeforces.com/problemset/problem/1894/A
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void solve() {
+	int n;
+	cin >> n;
+
+	string s;
+	cin >> s;
+
+	cout << s[n - 1] << "\n";
+}
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		solve();
+	}
+}
+
+
+/*
+7
+5
+ABBAA
+3
+BBB
+7
+BBAAABA
+20
+AAAAAAAABBBAABBBBBAB
+1
+A
+13
+AAAABABBABBAB
+7
+BBBAAAA
+
+*/
+
+/*
+A
+B
+A
+B
+A
+B
+A
+
+*/
